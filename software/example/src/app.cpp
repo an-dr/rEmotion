4c47652d91eb60a186e7daa47ae3a6f0e87aa9c2
Andrei Gramakov <mail@agramakov.me>
2024-06-08 20:23:23 +0200

    Add demo app

---
+++ software/example/src/app.cpp
@@ -0,0 +1,15 @@
+// *************************************************************************
+//
+// Copyright (c) 2024 Andrei Gramakov. All rights reserved.
+//
+// site:    https://agramakov.me
+// e-mail:  mail@agramakov.me
+//
+// *************************************************************************
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
