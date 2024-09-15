237e28552c9d298bf3d83ff2860de952d416cdae
Andrei Gramakov <mail@agramakov.me>
2024-09-15 15:36:59 +0200

    wip

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,34 +1,31 @@
 #include <Arduino.h>
-// #include <LibPrintf.h>
 
 #include "pinout.h"
 #include "display.h"
 #include "i2c.h"
 #include "serial.h"
+#include "ulog.h"
 #include "controlcallback.h"
 
-int putchar(char c, FILE *fp) {
-    if (c == '\n') {
-        putchar(char)'\r', fp);
-    }
-    Serial.write(c);
-    return 0;
+
+
+// Custom putchar function to redirect printf output to Serial
+extern "C" int putchar(int ch) {
+    Serial.write(ch); // Send the character to Serial
+    return ch;        // Return the character
 }
 
-// int getChar(FILE *fp) {
-//     while (!(Serial.available()))
-//         ;
-//     return (Serial.read());
-// }
+
+void ulog_callback(ulog_Event *ev, void *arg) {
+    static char buffer[128];
+    ulog_event_to_cstr(ev, buffer, sizeof(buffer));
+    ((SerialUSB *) arg)->println(buffer);
+}
 
 
-
-void setup(void) {
-    
-    Serial.begin(9600);
-    delay(5000);
-    // serial_init();
-    printf("Setup\n");
+void setup() {
+    ulog_add_callback(ulog_callback, (void* )&Serial, LOG_TRACE);
+    Serial.begin(9600); // Initialize serial communication at 9600 baud
     display_init();
     // i2c.Init(0x2c, 16, (void *)i2c_reqEv, (void *)i2c_rcvEv);
     // Cc.Init(connection,SIZE_ARR(connection));
@@ -36,12 +33,14 @@ void setup(void) {
     // greeting();
 }
 
-void loop(void) {
+void loop() {
+    static int a=0;
+    // Your main code here
+    log_info("Hello from Arduino's loop() %d!", a++); // Example usage of printf
     // blinking();
     // demo();
     // blinking();
-    printf("loop\n");
-    delay(3000);
     // control_poll();
     // i2c.Print();
+    delay(1000); // Delay for 1 second
 }
--- firmware/src/printf.cpp
+++ firmware/src/printf.cpp
@@ -7,16 +7,37 @@ int getChar(FILE *fp) {
     return (Serial.read());
 }
 
-int putChar(char c, FILE *fp) {
-    if (c == '\n') {
-        putChar((char)'\r', fp);
-    }
-    Serial.write(c);
-    return 0;
-}
 
-void printf_init(void)
-{
-    fdevopen(putChar, getChar);
-    // printf("[printf] Ready!\n");
-}
+// int __io_putchar(int ch)
+// {
+//     Serial.write(ch);
+//     return ch;
+// }
+
+// int _write(int file, char *ptr, int len)
+// {
+//     int DataIdx;
+//     for (DataIdx = 0; DataIdx < len; DataIdx++) { __io_putchar(*ptr++); }
+//     return len;
+// }
+// int putChar(char c, FILE *fp) {
+//     if (c == '\n') {
+//         putChar((char)'\r', fp);
+//     }
+//     Serial.write(c);
+//     return 0;
+// }
+
+// FILE my_stdout = {0};
+// FILE my_stdin = {0};
+
+// void printf_init(void)
+// {
+//     my_stdout._file = putChar;
+//     my_stdin._file = getChar;
+    
+    
+//     stdout = &my_stdout;
+//     stdin = &my_stdin;
+//     // printf("[printf] Ready!\n");
+// }
