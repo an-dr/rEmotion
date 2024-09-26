422f0a7a46d4f5ee3526164996bf5ad66a9a1f30
Andrei Gramakov <mail@agramakov.me>
2024-09-26 19:00:35 +0200

    wip

--- remotion_control/example/src/app.cpp
+++ remotion_control/example/src/app.cpp
@@ -7,20 +7,32 @@
 //
 // *************************************************************************
 
+#include <cstdlib>
+#include <iostream>
+#include <jpeglib.h>
 #include <linux/videodev2.h>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
-#include <jpeglib.h>
 #include <unistd.h>
-#include "Remotion.hpp"
+#include "RemotionCamera.hpp"
 #include "RemotionController.hpp"
 
+void my_error_exit(j_common_ptr cinfo) {
+    (void)cinfo;
+    // You can log the error or handle it as needed
+    // But do not call exit() to suppress the warning
+}
 
-static cv::Mat extract_frame_from_mjpeg(unsigned char* mjpeg_data, int width, int height)
-{
+static cv::Mat extract_frame_from_mjpeg(unsigned char *mjpeg_data, int width,
+                                        int height) {
+    // Error handling
+    jpeg_error_mgr jerr;
+    jerr.error_exit = my_error_exit;
+
     // Create a new jpeg_decompress_struct object
     jpeg_decompress_struct cinfo;
-    jpeg_error_mgr jerr;
+
+    // Suppress warnings
     cinfo.err = jpeg_std_error(&jerr);
 
     // Initialize the jpeg_decompress_struct object
@@ -41,15 +53,17 @@ static cv::Mat extract_frame_from_mjpeg(unsigned char* mjpeg_data, int width, in
     // Allocate memory for the output image
     jpeg_start_decompress(&cinfo);
 
-    // Create a new cv::Mat object with the same size and number of channels as the output image
+    // Create a new cv::Mat object with the same size and number of channels as
+    // the output image
     cv::Mat frame(height, width, CV_8UC3);
 
-    // Copy the pixel data from the jpeg_decompress_struct object to the cv::Mat object
+    // Copy the pixel data from the jpeg_decompress_struct object to the cv::Mat
+    // object
     JSAMPROW row_pointer[1];
     int row_stride = width * 3;
-    while (cinfo.output_scanline < cinfo.output_height)
-    {
-        row_pointer[0] = (JSAMPROW)frame.data + cinfo.output_scanline * row_stride;
+    while (cinfo.output_scanline < cinfo.output_height) {
+        row_pointer[0] =
+            (JSAMPROW)frame.data + cinfo.output_scanline * row_stride;
         jpeg_read_scanlines(&cinfo, row_pointer, 1);
     }
 
@@ -57,30 +71,46 @@ static cv::Mat extract_frame_from_mjpeg(unsigned char* mjpeg_data, int width, in
     jpeg_finish_decompress(&cinfo);
     jpeg_destroy_decompress(&cinfo);
 
+    // Convert mat color space from BGR to RGB
+    cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);
+
     // Return the cv::Mat object
     return frame;
 }
 
 static void write_img(char *buffer, int width, int height,
                       std::string filename) {
-    cv::Mat img = extract_frame_from_mjpeg((unsigned char *)buffer, width, height);
+    // decode mjpg buffer
+    cv::Mat img =
+        extract_frame_from_mjpeg((unsigned char *)buffer, width, height);
+
+    // cv::Mat img = cv::imdecode(cv::Mat(1, width * height * 3 / 2, CV_8UC1,
+    // buffer), cv::IMREAD_COLOR);
     cv::imwrite(filename, img);
-    // cv::imwrite(filename, img);
 }
 
-
 int main(int argc, char const *argv[]) {
+    (void)argc;
+    (void)argv;
+
     // Set parameters
-    const int width = 1280;
-    const int height = 720;
+    const int width = 640;
+    const int height = 480;
     const int fps = 30;
-    const unsigned int format = V4L2_PIX_FMT_MJPEG; // check via v4l2-ctl --list-formats-ext
+    const unsigned int format =
+        V4L2_PIX_FMT_MJPEG;  // check via v4l2-ctl --list-formats-ext
     std::string video_dev = "/dev/video0";
     std::string port_name = "/dev/ttyACM0";
 
+    Re::RemotionCamera cam(video_dev, format, width, height, fps);
+    auto vidbuf_size = cam.getVideoCaptureBufferSize();
+    if (vidbuf_size == -1) {
+        return -1;
+    }
+    char *vidbuf = new char[vidbuf_size];
+
     // Create and init Remotion object
     Re::RemotionController ctrl(port_name);
-    
     ctrl.setExpression(RemotionExpression::CONFUSED);
     sleep(1);
     ctrl.setExpression(RemotionExpression::CALM);
@@ -88,32 +118,10 @@ int main(int argc, char const *argv[]) {
     ctrl.setExpression(RemotionExpression::HAPPY);
     sleep(1);
 
-    // // Create and init Remotion object
-    // Remotion r;
-    // auto buffer_size = r.getVideoCaptureBufferSize( video_dev, format, width, height, fps);
-    // if (buffer_size == -1) {
-    //     return -1;
-    // }
-    // char *buffer = new char[buffer_size];
-    // r.start(port_name, video_dev, format, width, height, fps);
-
-
-    // // Set different expressions
-    // r.setExpression(RemotionExpression::CONFUSED);
-    // sleep(1);
-    // r.setExpression(RemotionExpression::CALM);
-    // sleep(1);
-    // r.setExpression(RemotionExpression::HAPPY);
-    // sleep(1);
-    
-    // // Read video frames and save them to img.jpg
-    // while (true) {
-    //     r.readVideoFrame(buffer, buffer_size);
-    //     write_img(buffer, width, height, "img.jpg");
-    //     sleep(1 / fps);
-    // }
-    
-    // r.stop();
-    // delete buffer;
-    // return -1;
+    // Read video frames and save them to img.jpg
+    while (true) {
+        cam.readVideoFrame(vidbuf, vidbuf_size);
+        write_img(vidbuf, width, height, "img.jpg");
+        sleep(1 / fps);
+    }
 }
---
+++ remotion_control/src/RemotionCamera.cpp
@@ -0,0 +1,66 @@
+// *************************************************************************
+//
+// Copyright (c) 2024 Andrei Gramakov. All rights reserved.
+//
+// site:    https://agramakov.me
+// e-mail:  mail@agramakov.me
+//
+// *************************************************************************
+#include <cstdio>
+#include <stdexcept>
+#include <string>
+
+#include "RemotionError.hpp"
+#include "RemotionExpression.hpp"
+#include "RemotionStatus.hpp"
+#include "V4l2Capture.h"
+
+#include "RemotionCamera.hpp"
+
+using namespace Re;
+
+RemotionCamera::RemotionCamera(const std::string &video_device,
+                               unsigned int v4l2_pix_fmt, int width, int height,
+                               int fps) {
+    videoCaptureParams = new V4L2DeviceParameters(
+        video_device.c_str(), v4l2_pix_fmt, width, height, fps);
+    videoCapture = V4l2Capture::create(*videoCaptureParams);
+    if (videoCapture == nullptr) {
+        throw std::runtime_error("Failed to create V4l2Capture object");
+    }
+}
+
+RemotionCamera::~RemotionCamera() {
+    if (videoCapture != nullptr) {
+        delete videoCapture;
+    }
+}
+
+int RemotionCamera::getVideoCaptureBufferSize() {
+    if (videoCapture == nullptr) {
+        return -1;
+    }
+    return videoCapture->getBufferSize();
+}
+
+RemotionError RemotionCamera::readVideoFrame(char *out_buffer,
+                                             size_t out_buffer_size) {
+    if (videoCapture == nullptr) {
+        return RemotionError::ERROR_OPEN_PORT;
+    }
+
+    timeval tv{1, 0}; // 1 second, 0 microseconds
+
+    // If the camera is not opened, return an error
+    if (!videoCapture->isReadable(&tv)) {
+        return RemotionError::ERROR_READ_PORT;
+    }
+
+    // Read the frame from the camera
+    int rsize = videoCapture->read(out_buffer, out_buffer_size);
+    if (rsize == -1) {
+        return RemotionError::ERROR_READ_IMAGE;
+    }
+
+    return RemotionError::NO_ERROR;
+}
